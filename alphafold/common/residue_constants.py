92955238e5e61ef5a6ccdaf94faf7090377dfc29
Diego Alvarez S <dialvarezs@gmail.com>
2021-07-20 19:05:17 -0300

    Read repo base dir from env variable

--- alphafold/common/residue_constants.py
+++ alphafold/common/residue_constants.py
@@ -16,6 +16,7 @@
 
 import collections
 import functools
+import os
 from typing import Mapping, List, Tuple
 
 import numpy as np
@@ -402,8 +403,10 @@ def load_stereo_chemical_props() -> Tuple[Mapping[str, List[Bond]],
     residue_virtual_bonds: dict that maps resname --> list of Bond tuples
     residue_bond_angles: dict that maps resname --> list of BondAngle tuples
   """
-  stereo_chemical_props_path = (
-      'alphafold/common/stereo_chemical_props.txt')
+  stereo_chemical_props_path = os.path.join(
+    os.environ.get('ALPHAFOLD_BASE_DIR'),
+    'alphafold/common/stereo_chemical_props.txt'
+  )
   with open(stereo_chemical_props_path, 'rt') as f:
     stereo_chemical_props = f.read()
   lines_iter = iter(stereo_chemical_props.splitlines())
